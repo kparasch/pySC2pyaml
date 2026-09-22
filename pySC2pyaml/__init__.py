51010185f1da831247ccb57e03b0ede56244da30
kparasch <konstantinos.paraschou@desy.de>
2026-09-22 15:27:15 +0200

    bugfix in pyproject.toml

--- pySC2pyaml/__init__.py
+++ pySC2pyaml/__init__.py
@@ -8,6 +8,5 @@ from .version import __version__
 __title__ = "pySC2pyaml"
 __description__ = "Bindings between pySC (control system) and PyAML"
 #__url__ = "https://github.com/python-accelerator-middle-layer/pyaml"
-__version__ = "0.0.0"
 __author__ = "Konstantinos Paraschou"
 __author_email__ = ""
